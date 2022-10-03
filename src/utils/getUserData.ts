6c8f4c3408dc3f2dbb792bdd5373114084d0d2ba
Arkadiy_P <asdfzxcv53@mail.ru>
2022-10-03 18:52:01 +0300

    clear

--- src/utils/getUserData.ts
+++
@@ -1,9 +0,0 @@
-export const getUserData = () => {
-    const item = localStorage.getItem('user');
-    let user;
-    if (item) {
-        user = JSON.parse(item);
-    }
-    return user;
-};
-export const getAvatar = () => localStorage.getItem('avatarIcon');
